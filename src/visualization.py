b671de748612d40f25edbff6bbd1f0e48ab767ea
gunesevitan <gunesevitan@gmail.com>
2022-01-28 16:37:54 +0300

    viz learning curve

--- src/visualization.py
+++ src/visualization.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -66,6 +67,46 @@ def visualize_predictions(y_true, y_pred, title, path=None):
     axes[1].set_title(f'Predictions Distributions', size=20, pad=15)
 
     fig.suptitle(title, size=20, y=0.95)
+
+    if path is None:
+        plt.show()
+    else:
+        plt.savefig(path)
+        plt.close(fig)
+
+
+def visualize_learning_curve(training_losses, validation_losses, title, path=None):
+
+    """
+    Visualize learning curves of the models
+
+    Parameters
+    ----------
+    training_losses [array-like of shape (n_epochs)]: Array of training losses computed after every epoch
+    validation_losses [array-like of shape (n_epochs)]: Array of validation losses computed after every epoch
+    title (str): Title of the plot
+    path (str or None): Path of the output file (if path is None, plot is displayed with selected backend)
+    """
+
+    fig, ax = plt.subplots(figsize=(32, 8), dpi=100)
+    sns.lineplot(
+        x=np.arange(1, len(training_losses) + 1),
+        y=training_losses,
+        ax=ax,
+        label='train_loss'
+    )
+    sns.lineplot(
+        x=np.arange(1, len(validation_losses) + 1),
+        y=validation_losses,
+        ax=ax,
+        label='val_loss'
+    )
+    ax.set_xlabel('Epochs/Steps', size=15, labelpad=12.5)
+    ax.set_ylabel('Loss', size=15, labelpad=12.5)
+    ax.tick_params(axis='x', labelsize=12.5, pad=10)
+    ax.tick_params(axis='y', labelsize=12.5, pad=10)
+    ax.legend(prop={'size': 18})
+    ax.set_title(title, size=20, pad=15)
 
     if path is None:
         plt.show()
