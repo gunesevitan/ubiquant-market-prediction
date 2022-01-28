82bfa7cb03814dae8ba9944890af869d1eb79fbd
gunesevitan <gunesevitan@gmail.com>
2022-01-28 18:36:24 +0300

    nn main

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 import settings
 from lgb_trainer import LightGBMTrainer
+from nn_trainer import NeuralNetworkTrainer
 
 
 if __name__ == '__main__':
@@ -27,6 +28,21 @@ if __name__ == '__main__':
             model_parameters=config['model_parameters'],
             fit_parameters=config['fit_parameters'],
             categorical_features=config['categorical_features']
+        )
+
+        if args.mode == 'train':
+            if config['validation_type'] == 'single_split':
+                trainer.train_and_validate_single_split(df)
+            elif config['validation_type'] == 'no_split':
+                trainer.train_no_split(df)
+
+    elif config['model'] == 'neural_network':
+
+        trainer = NeuralNetworkTrainer(
+            features=config['features'],
+            target=config['target'],
+            model_parameters=config['model_parameters'],
+            training_parameters=config['training_parameters']
         )
 
         if args.mode == 'train':
