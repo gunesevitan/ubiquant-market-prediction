436cb2461c93ecb4541fc51b39d37c510b66ff22
gunesevitan <gunesevitan@gmail.com>
2022-02-02 16:56:26 +0300

    main linear model

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import yaml
 import pandas as pd
 
 import settings
+from linear_model_trainer import LinearModelTrainer
 from lgb_trainer import LightGBMTrainer
 from lgb_ranker import LightGBMRanker
 from nn_trainer import NeuralNetworkTrainer
@@ -21,7 +22,22 @@ if __name__ == '__main__':
     if config['validation_type'] == 'single_split':
         df = df.merge(pd.read_pickle(settings.DATA / 'folds.pkl'), on=['time_id', 'investment_id'], how='left')
 
-    if config['model'] == 'lightgbm':
+    if config['model'] == 'linear_model':
+
+        trainer = LinearModelTrainer(
+            features=config['features'],
+            target=config['target'],
+            model_class=config['model_class'],
+            model_parameters=config['model_parameters']
+        )
+
+        if args.mode == 'train':
+            if config['validation_type'] == 'single_split':
+                trainer.train_and_validate_single_split(df)
+            elif config['validation_type'] == 'no_split':
+                trainer.train_no_split(df)
+
+    elif config['model'] == 'lightgbm':
 
         trainer = LightGBMTrainer(
             features=config['features'],
