91b611d71cd39f70d6711d077105ebc4100e1502
gunesevitan <gunesevitan@gmail.com>
2022-02-22 16:15:46 +0300

    lgb and xgb trainers refactored

--- src/lgb_trainer.py
+++ src/lgb_trainer.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
@@ -9,13 +10,15 @@ import visualization
 
 class LightGBMTrainer:
 
-    def __init__(self, features, target, model_parameters, fit_parameters, categorical_features):
+    def __init__(self, features, target, model_parameters, fit_parameters, categorical_features, seeds, model_directory):
 
         self.features = features
         self.target = target
         self.model_parameters = model_parameters
         self.fit_parameters = fit_parameters
         self.categorical_features = categorical_features
+        self.seeds = seeds
+        self.model_directory = model_directory
 
     def train_and_validate_single_split(self, df):
 
@@ -29,43 +32,69 @@ class LightGBMTrainer:
 
         print(f'{"-" * 30}\nRunning LightGBM Model for Training\n{"-" * 30}\n')
 
-        df_feature_importance = pd.DataFrame(data=np.zeros(len(self.features)), index=self.features, columns=['Importance'])
+        # Create directory to save models and training results
+        Path(settings.MODELS / self.model_directory).mkdir(parents=True, exist_ok=True)
+
         trn_idx, val_idx = df.loc[df['fold'] == 0].index, df.loc[df['fold'] == 1].index
         trn_dataset = lgb.Dataset(df.loc[trn_idx, self.features], label=df.loc[trn_idx, self.target], categorical_feature=self.categorical_features)
         val_dataset = lgb.Dataset(df.loc[val_idx, self.features], label=df.loc[val_idx, self.target], categorical_feature=self.categorical_features)
+        seed_avg_val_predictions = np.zeros_like(df.loc[val_idx, self.target])
+        df_feature_importance = pd.DataFrame(data=np.zeros(len(self.features)), index=self.features, columns=['Importance'])
 
-        model = lgb.train(
-            params=self.model_parameters,
-            train_set=trn_dataset,
-            valid_sets=[trn_dataset, val_dataset],
-            num_boost_round=self.fit_parameters['boosting_rounds'],
-            early_stopping_rounds=self.fit_parameters['early_stopping_rounds'],
-            verbose_eval=self.fit_parameters['verbose_eval'],
-            feval=[metrics.pearson_correlation_coefficient_eval_lgb]
-        )
-        model.save_model(
-            settings.MODELS / 'lightgbm' / 'single_split' / 'model',
-            num_iteration=None,
-            start_iteration=0,
-            importance_type='gain'
-        )
+        for seed in self.seeds:
 
-        df.loc[val_idx, 'predictions'] = model.predict(df.loc[val_idx, self.features])
-        df_feature_importance['Importance'] += model.feature_importance(importance_type='gain')
-        val_score = metrics.mean_pearson_correlation_coefficient(df)
-        print(f'\nLightGBM Validation Score: {val_score:.6f}\n')
-        df['predictions'].to_csv(settings.MODELS / 'lightgbm' / 'single_split' / 'predictions.csv', index=False)
+            print(f'\nSeed {seed}\n')
+            self.model_parameters['seed'] = seed
+            self.model_parameters['feature_fraction_seed'] = seed
+            self.model_parameters['bagging_seed'] = seed
+            self.model_parameters['drop_seed'] = seed
+            self.model_parameters['data_random_seed'] = seed
 
+            model = lgb.train(
+                params=self.model_parameters,
+                train_set=trn_dataset,
+                valid_sets=[trn_dataset, val_dataset],
+                num_boost_round=self.fit_parameters['boosting_rounds'],
+                callbacks=[
+                    lgb.early_stopping(self.fit_parameters['early_stopping_rounds']),
+                    lgb.log_evaluation(self.fit_parameters['verbose_eval'])
+                ],
+                feval=[metrics.pearson_correlation_coefficient_eval_lgb]
+            )
+            # Save trained model
+            model.save_model(
+                settings.MODELS / self.model_directory / 'single_split' / f'model_seed{seed}.txt',
+                num_iteration=None,
+                start_iteration=0,
+                importance_type='gain'
+            )
+
+            # Save validation predictions and feature importance scaled by the number of seeds
+            val_predictions = model.predict(df.loc[val_idx, self.features])
+            seed_avg_val_predictions += (val_predictions / len(self.seeds))
+            df.loc[val_idx, 'predictions'] = val_predictions
+            df_feature_importance['Importance'] += (model.feature_importance(importance_type='gain') / len(self.seeds))
+
+            # Calculate mean pearson correlation coefficient on validation set
+            val_score = metrics.mean_pearson_correlation_coefficient(df.loc[val_idx, :])
+            print(f'\nLightGBM Validation Score: {val_score:.6f} (Seed: {seed})\n')
+
+        df.loc[val_idx, 'predictions'] = seed_avg_val_predictions
+        val_score = metrics.mean_pearson_correlation_coefficient(df.loc[val_idx, :])
+        print(f'\nLightGBM Validation Score: {val_score:.6f}\n ({len(self.seeds)} Seed Average)')
+        df['predictions'].to_csv(settings.MODELS / self.model_directory / 'single_split' / 'predictions.csv', index=False)
+
+        # Visualize feature importance and predictions
         visualization.visualize_feature_importance(
             df_feature_importance=df_feature_importance,
             title='LightGBM - Single Split Feature Importance (Gain)',
-            path=settings.MODELS / 'lightgbm' / 'single_split' / 'feature_importance.png'
+            path=settings.MODELS / self.model_directory / 'single_split' / 'feature_importance.png'
         )
         visualization.visualize_predictions(
             y_true=df.loc[val_idx, self.target],
             y_pred=df.loc[val_idx, 'predictions'],
             title='LightGBM - Single Split Predictions',
-            path=settings.MODELS / 'lightgbm' / 'single_split' / 'predictions.png'
+            path=settings.MODELS / self.model_directory / 'single_split' / 'predictions.png'
         )
 
     def train_no_split(self, df):
@@ -80,28 +109,46 @@ class LightGBMTrainer:
 
         print(f'{"-" * 30}\nRunning LightGBM Model for Training\n{"-" * 30}\n')
 
-        df_feature_importance = pd.DataFrame(data=np.zeros(len(self.features)), index=self.features, columns=['Importance'])
+        # Create directory to save models and training results
+        Path(settings.MODELS / self.model_directory).mkdir(parents=True, exist_ok=True)
+
         trn_dataset = lgb.Dataset(df.loc[:, self.features], label=df.loc[:, self.target], categorical_feature=self.categorical_features)
+        df_feature_importance = pd.DataFrame(data=np.zeros(len(self.features)), index=self.features, columns=['Importance'])
 
-        model = lgb.train(
-            params=self.model_parameters,
-            train_set=trn_dataset,
-            valid_sets=[trn_dataset],
-            num_boost_round=self.fit_parameters['boosting_rounds'],
-            early_stopping_rounds=self.fit_parameters['early_stopping_rounds'],
-            verbose_eval=self.fit_parameters['verbose_eval'],
-            feval=[metrics.pearson_correlation_coefficient_eval_lgb]
-        )
-        model.save_model(
-            settings.MODELS / 'lightgbm' / 'no_split' / 'model',
-            num_iteration=None,
-            start_iteration=0,
-            importance_type='gain'
-        )
+        for seed in self.seeds:
 
-        df_feature_importance['Importance'] += model.feature_importance(importance_type='gain')
+            print(f'\nSeed {seed}\n')
+            self.model_parameters['seed'] = seed
+            self.model_parameters['feature_fraction_seed'] = seed
+            self.model_parameters['bagging_seed'] = seed
+            self.model_parameters['drop_seed'] = seed
+            self.model_parameters['data_random_seed'] = seed
+
+            model = lgb.train(
+                params=self.model_parameters,
+                train_set=trn_dataset,
+                valid_sets=[trn_dataset],
+                num_boost_round=self.fit_parameters['boosting_rounds'],
+                callbacks=[
+                    lgb.early_stopping(self.fit_parameters['early_stopping_rounds']),
+                    lgb.log_evaluation(self.fit_parameters['verbose_eval'])
+                ],
+                feval=[metrics.pearson_correlation_coefficient_eval_lgb]
+            )
+            # Save trained model
+            model.save_model(
+                settings.MODELS / self.model_directory / 'no_split' / f'model_seed{seed}.txt',
+                num_iteration=None,
+                start_iteration=0,
+                importance_type='gain'
+            )
+
+            # Save feature importance scaled by the number of seeds
+            df_feature_importance['Importance'] += (model.feature_importance(importance_type='gain') / len(self.seeds))
+
+        # Visualize feature importance
         visualization.visualize_feature_importance(
             df_feature_importance=df_feature_importance,
             title='LightGBM - No Split Feature Importance (Gain)',
-            path=settings.MODELS / 'lightgbm' / 'no_split' / 'feature_importance.png'
+            path=settings.MODELS / self.model_directory / 'no_split' / 'feature_importance.png'
         )
--- src/xgb_trainer.py
+++ src/xgb_trainer.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import numpy as np
 import pandas as pd
 import xgboost as xgb
@@ -9,12 +10,14 @@ import visualization
 
 class XGBoostTrainer:
 
-    def __init__(self, features, target, model_parameters, fit_parameters):
+    def __init__(self, features, target, model_parameters, fit_parameters, seeds, model_directory):
 
         self.features = features
         self.target = target
         self.model_parameters = model_parameters
         self.fit_parameters = fit_parameters
+        self.seeds = seeds
+        self.model_directory = model_directory
 
     def train_and_validate_single_split(self, df):
 
@@ -28,34 +31,53 @@ class XGBoostTrainer:
 
         print(f'{"-" * 30}\nRunning XGBoost Model for Training\n{"-" * 30}\n')
 
-        df_feature_importance = pd.DataFrame(data=np.zeros(len(self.features)), index=self.features, columns=['Importance'])
+        # Create directory to save models and training results
+        Path(settings.MODELS / self.model_directory).mkdir(parents=True, exist_ok=True)
+
         trn_idx, val_idx = df.loc[df['fold'] == 0].index, df.loc[df['fold'] == 1].index
         trn_dataset = xgb.DMatrix(df.loc[trn_idx, self.features], label=df.loc[trn_idx, self.target])
         val_dataset = xgb.DMatrix(df.loc[val_idx, self.features], label=df.loc[val_idx, self.target])
+        seed_avg_val_predictions = np.zeros_like(df.loc[val_idx, self.target])
+        df_feature_importance = pd.DataFrame(data=np.zeros(len(self.features)), index=self.features, columns=['Importance'])
 
-        model = xgb.train(
-            params=self.model_parameters,
-            dtrain=trn_dataset,
-            evals=[(trn_dataset, 'train'), (val_dataset, 'val')],
-            num_boost_round=self.fit_parameters['boosting_rounds'],
-            early_stopping_rounds=self.fit_parameters['early_stopping_rounds'],
-            verbose_eval=self.fit_parameters['verbose_eval'],
-            feval=metrics.pearson_correlation_coefficient_eval_xgb
-        )
-        model.save_model(settings.MODELS / 'xgboost' / 'single_split' / 'model.json')
+        for seed in self.seeds:
 
-        df.loc[val_idx, 'predictions'] = model.predict(xgb.DMatrix(df.loc[val_idx, self.features]))
-        for feature, importance in model.get_score(importance_type='gain').items():
-            df_feature_importance.loc[feature, 'Importance'] += importance
+            print(f'\nSeed {seed}\n')
+            self.model_parameters['seed'] = seed
 
-        val_score = metrics.mean_pearson_correlation_coefficient(df)
-        print(f'\nXGBoost Validation Score: {val_score:.6f}\n')
-        df['predictions'].to_csv(settings.MODELS / 'xgboost' / 'single_split' / 'predictions.csv', index=False)
+            model = xgb.train(
+                params=self.model_parameters,
+                dtrain=trn_dataset,
+                evals=[(trn_dataset, 'train'), (val_dataset, 'val')],
+                num_boost_round=self.fit_parameters['boosting_rounds'],
+                early_stopping_rounds=self.fit_parameters['early_stopping_rounds'],
+                verbose_eval=self.fit_parameters['verbose_eval'],
+                feval=metrics.pearson_correlation_coefficient_eval_xgb
+            )
+            # Save trained model
+            model.save_model(settings.MODELS / self.model_directory / 'single_split' / f'model_seed{seed}.json')
 
+            # Save validation predictions and feature importance scaled by the number of seeds
+            val_predictions = model.predict(xgb.DMatrix(df.loc[val_idx, self.features]))
+            seed_avg_val_predictions += (val_predictions / len(self.seeds))
+            df.loc[val_idx, 'predictions'] = val_predictions
+            for feature, importance in model.get_score(importance_type='gain').items():
+                df_feature_importance.loc[feature, 'Importance'] += (importance / len(self.seeds))
+
+            # Calculate mean pearson correlation coefficient on validation set
+            val_score = metrics.mean_pearson_correlation_coefficient(df)
+            print(f'\nXGBoost Validation Score: {val_score:.6f} (Seed: {seed})\n')
+
+        df.loc[val_idx, 'predictions'] = seed_avg_val_predictions
+        val_score = metrics.mean_pearson_correlation_coefficient(df.loc[val_idx, :])
+        print(f'\nXGBoost Validation Score: {val_score:.6f}\n ({len(self.seeds)} Seed Average)')
+        df['predictions'].to_csv(settings.MODELS / self.model_directory / 'single_split' / 'predictions.csv', index=False)
+
+        # Visualize feature importance and predictions
         visualization.visualize_feature_importance(
             df_feature_importance=df_feature_importance,
             title='XGBoost - Single Split Feature Importance (Gain)',
-            path=settings.MODELS / 'xgboost' / 'single_split' / 'feature_importance.png'
+            path=settings.MODELS / self.model_directory / 'single_split' / 'feature_importance.png'
         )
         visualization.visualize_predictions(
             y_true=df.loc[val_idx, self.target],
@@ -76,24 +98,36 @@ class XGBoostTrainer:
 
         print(f'{"-" * 30}\nRunning XGBoost Model for Training\n{"-" * 30}\n')
 
-        df_feature_importance = pd.DataFrame(data=np.zeros(len(self.features)), index=self.features, columns=['Importance'])
+        # Create directory to save models and training results
+        Path(settings.MODELS / self.model_directory).mkdir(parents=True, exist_ok=True)
+
         trn_dataset = xgb.DMatrix(df.loc[:, self.features], label=df.loc[:, self.target])
+        df_feature_importance = pd.DataFrame(data=np.zeros(len(self.features)), index=self.features, columns=['Importance'])
 
-        model = xgb.train(
-            params=self.model_parameters,
-            dtrain=trn_dataset,
-            evals=[(trn_dataset, 'train')],
-            num_boost_round=self.fit_parameters['boosting_rounds'],
-            early_stopping_rounds=self.fit_parameters['early_stopping_rounds'],
-            verbose_eval=self.fit_parameters['verbose_eval'],
-            feval=metrics.pearson_correlation_coefficient_eval_xgb
-        )
-        model.save_model(settings.MODELS / 'xgboost' / 'no_split' / 'model.json')
+        for seed in self.seeds:
 
-        for feature, importance in model.get_score(importance_type='gain').items():
-            df_feature_importance.loc[feature, 'Importance'] += importance
+            print(f'\nSeed {seed}\n')
+            self.model_parameters['seed'] = seed
+
+            model = xgb.train(
+                params=self.model_parameters,
+                dtrain=trn_dataset,
+                evals=[(trn_dataset, 'train')],
+                num_boost_round=self.fit_parameters['boosting_rounds'],
+                early_stopping_rounds=self.fit_parameters['early_stopping_rounds'],
+                verbose_eval=self.fit_parameters['verbose_eval'],
+                feval=metrics.pearson_correlation_coefficient_eval_xgb
+            )
+            # Save trained model
+            model.save_model(settings.MODELS / self.model_directory / 'no_split' / f'model_seed{seed}.json')
+
+            # Save feature importance scaled by the number of seeds
+            for feature, importance in model.get_score(importance_type='gain').items():
+                df_feature_importance.loc[feature, 'Importance'] += (importance / len(self.seeds))
+
+        # Visualize feature importance
         visualization.visualize_feature_importance(
             df_feature_importance=df_feature_importance,
             title='XGBoost - No Split Feature Importance (Gain)',
-            path=settings.MODELS / 'xgboost' / 'no_split' / 'feature_importance.png'
+            path=settings.MODELS / self.model_directory / 'no_split' / 'feature_importance.png'
         )
